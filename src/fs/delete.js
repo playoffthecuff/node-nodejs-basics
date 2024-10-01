d11ab6bc346102350776837daf727b79884385e3
playoffthecuff <jmobile@bk.ru>
2024-10-01 19:21:12 +0300

    feat: add fs/delete solution

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,27 @@
+import path from "node:path";
+import fs from "node:fs";
+
 const remove = async () => {
-    // Write your code here 
+  const eMsg = "FS operation failed";
+
+  const dir = path.resolve("files");
+  const src = path.join(dir, "fileToRemove.txt");
+
+  if (!fs.existsSync(src)) throw new Error(eMsg);
+
+  fs.rm(src, (e) => {
+    if (e) {
+      console.error(e);
+    } else {
+      console.log("file removed successfully");
+    }
+  });
 };
 
 await remove();
+
+/*
+ * delete.js - implement function that deletes file fileToRemove.txt
+ * (if there's no file fileToRemove.txt
+ * Error with message FS operation failed must be thrown)
+ */
