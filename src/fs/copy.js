4089a2b26c9f8b7bdf4e01865e404625a2356176
playoffthecuff <jmobile@bk.ru>
2024-10-01 08:43:23 +0300

    feat: add fs/copy solution

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,28 @@
+import path from "node:path";
+import fs from "node:fs";
+
 const copy = async () => {
-    // Write your code here 
+  const eMsg = "FS operation failed";
+
+  const src = path.resolve("files");
+  const dest = path.resolve("files_copy");
+
+  if (!fs.existsSync(src) || fs.existsSync(dest)) throw new Error(eMsg);
+  
+  fs.cp(src, dest, {recursive: true}, (err) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log("folder copied successfully");
+    }
+  });
 };
 
 await copy();
+
+/*
+ * implement function that copies folder files files with all its content
+ * into folder files_copy at the same level
+ * (if files folder doesn't exists or files_copy has already been created
+ * Error with message FS operation failed must be thrown)
+ */
