881d3e07efb324f7eb0b40b1387e82251575280c
playoffthecuff <jmobile@bk.ru>
2024-10-02 02:13:52 +0300

    feat: add cli/args solution

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,13 @@
 const parseArgs = () => {
-    // Write your code here 
+  console.log(process.argv.slice(2).reduce((a,v, i) => a + (i & 1 ? ' is ' + v + ', ' : v.slice(2)), '').slice(0, -2))
 };
 
 parseArgs();
+
+/*
+*
+* implement function that parses command line arguments
+* (given in format --propName value --prop2Name value2, you don't need to validate it)
+* and prints them to the console in the format propName is value, prop2Name is value2
+*
+*/
