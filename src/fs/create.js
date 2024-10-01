b65573bc1de97225f2323784b64236026a33dd8e
playoffthecuff <jmobile@bk.ru>
2024-10-01 07:48:57 +0300

    feat: add fs/create solution

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,29 @@
+import path from "node:path"
+import fs from "node:fs"
+
 const create = async () => {
-    // Write your code here 
+
+  const dir = path.resolve('files')
+  const targetPath = path.join(dir, 'fresh.txt')
+
+  const content = 'I am fresh and young'
+  const eMessage = 'FS operation failed'
+
+  if (fs.existsSync(targetPath)) throw new Error(eMessage)
+
+  fs.writeFile(targetPath, content, err => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log('file written successfully');
+    }
+  });
 };
 
 await create();
+
+/*
+ * create.js - implement function that creates new file fresh.txt with content
+ * I am fresh and young inside of the files folder
+ * (if file already exists Error with message FS operation failed must be thrown)
+ */
