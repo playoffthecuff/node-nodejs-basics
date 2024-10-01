2f92798beba207031331ed2f571a0640dd192d03
playoffthecuff <jmobile@bk.ru>
2024-10-01 19:37:14 +0300

    feat: add fs/list solution

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,21 @@
+import path from "node:path";
+import fs from "node:fs";
+
 const list = async () => {
-    // Write your code here 
+  const eMsg = 'FS operation failed'
+
+  const dir = path.resolve('files')
+
+  if (!fs.existsSync(dir)) throw new Error(eMsg)
+
+  fs.readdirSync(dir).forEach(f => console.log(f))
 };
 
 await list();
+
+/*
+ *
+ * implement function that prints all array of filenames from files folder into console
+ * (if files folder doesn't exists Error with message FS operation failed must be thrown)
+ *
+ */
