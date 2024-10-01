15b7448bee2619c91d178a3c23908eaf573195e3
playoffthecuff <jmobile@bk.ru>
2024-10-02 01:34:33 +0300

    feat: add cli/env solution

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,12 @@
 const parseEnv = () => {
-    // Write your code here 
+  console.log(Object.entries(process.env).map(([k,v]) => 'RSS_' + k + '=' + v).join('; '))
 };
 
 parseEnv();
+
+/*
+*
+* implement function that parses environment variables with prefix RSS_
+* and prints them to the console in the format RSS_name1=value1; RSS_name2=value2
+*
+*/
