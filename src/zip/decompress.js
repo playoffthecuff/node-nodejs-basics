8c5ca96315d37b717bb66ec9e666a6d843e6c625
playoffthecuff <jmobile@bk.ru>
2024-10-02 22:54:56 +0300

    feat: add zip/decompress solution

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,17 @@
+import {createReadStream, createWriteStream} from "node:fs"
+import {resolve} from "node:path"
+import {createGunzip} from "node:zlib"
+import { pipeline } from "node:stream";
+
 const decompress = async () => {
-    // Write your code here 
+  pipeline(createReadStream(resolve('files/archive.gz')), createGunzip(), createWriteStream(resolve('files/fileToCompress.txt')), e => {if (e) {console.error(e)}; process.exitCode = 1})
 };
 
 await decompress();
+
+/*
+*
+* implement function that decompresses archive.gz back to the fileToCompress.txt
+* with same content as before compression using zlib and Streams API
+*
+*/
