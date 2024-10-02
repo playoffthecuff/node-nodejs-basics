9a390d4513a026b5b6583734c0e68d70b144e806
playoffthecuff <jmobile@bk.ru>
2024-10-02 22:36:20 +0300

    feat: add zip/compress solution

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,19 @@
+import fs from "node:fs"
+import path from "node:path"
+import {createGzip} from "node:zlib"
+import {pipeline} from "node:stream"
+
 const compress = async () => {
-    // Write your code here 
+  pipeline(fs.createReadStream(path
+    .resolve('files/fileToCompress.txt')), createGzip(), fs.createWriteStream(path.resolve('files/archive.gz')),
+    (e) => {if (e) {console.error(e); process.exitCode = 1}})
 };
 
 await compress();
+
+/*
+*
+* implement function that compresses file fileToCompress.txt
+* to archive.gz using zlib and Streams API
+*
+*/
