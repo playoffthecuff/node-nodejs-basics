811877b456231038ec75d39cc13dea8cc5054d68
playoffthecuff <jmobile@bk.ru>
2024-10-02 20:29:01 +0300

    feat: add streams/write solution

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,16 @@
+import {createWriteStream} from "node:fs"
+import {stdin} from "node:process"
+import {resolve} from "node:path"
+
 const write = async () => {
-    // Write your code here 
+  stdin.pipe(createWriteStream(resolve('files/fileToWrite.txt')))
 };
 
 await write();
+
+/*
+*
+* write.js - implement function that writes process.stdin data
+* into file fileToWrite.txt content using Writable Stream
+*
+*/
