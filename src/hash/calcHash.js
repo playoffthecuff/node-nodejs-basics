4c0085911be366c64d2c76e10defeffe84fe24d1
playoffthecuff <jmobile@bk.ru>
2024-10-02 07:50:02 +0300

    feat: add hash/calcHash solution

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,24 @@
+import { createHash } from "node:crypto"
+import fs from "node:fs"
+import path from "node:path"
+import process from "node:process"
+
 const calculateHash = async () => {
-    // Write your code here 
+  const dir = path.resolve('files')
+  const src = path.join(dir, 'fileToCalculateHashFor.txt')
+  console.log(src)
+
+  const hash = createHash("sha256")
+  const input = fs.createReadStream(src)
+
+  input.pipe(hash).setEncoding('hex').pipe(process.stdout)
 };
 
 await calculateHash();
+
+/*
+ *
+ * implement function that calculates SHA256 hash for file fileToCalculateHashFor.txt
+ * and logs it into console as hex using Streams API
+ *
+ */
