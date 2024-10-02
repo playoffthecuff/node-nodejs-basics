dbe4419d9dcb62b59532643c1373d2bdfeb4426f
playoffthecuff <jmobile@bk.ru>
2024-10-02 19:55:36 +0300

    feat: add streams/read solution

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,19 @@
+import process from "node:process"
+import fs from "node:fs"
+import path from "node:path"
+
 const read = async () => {
-    // Write your code here 
+  const dir = path.resolve("files")
+  const src = path.join(dir, "fileToRead.txt")
+
+  const input = fs.createReadStream(src)
+  input.pipe(process.stdout)
 };
 
 await read();
+
+/*
+ * implement function that reads file fileToRead.txt content
+ * using Readable Stream and prints it's content into process.stdout
+ *
+ */
