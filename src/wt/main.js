52cb7eeb7875daa40bdb9148262a8927f2182719
playoffthecuff <jmobile@bk.ru>
2024-10-03 07:23:07 +0300

    feat: add wt solution

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,18 @@
+import {Worker} from "node:worker_threads"
+import {resolve} from "node:path"
+import {availableParallelism} from "node:os"
+
 const performCalculations = async () => {
-    // Write your code here
+  const cores = availableParallelism()
+  let start = 10
+  const workers = Array
+    .from(Array(cores), _ => new Worker(resolve('worker.js'), {workerData: start++}))
+  const promises = workers
+    .map(w => new Promise((res, rej) => {
+      w.on('message', v => res({status: 'resolved', data: v}))
+      w.on('error', e => res({status: 'rejected', data: null}))
+    }))
+  Promise.all(promises).then(console.log)
 };
 
 await performCalculations();
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,20 @@
+import { workerData, parentPort } from 'node:worker_threads';
+
 // n should be received from main thread
 const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
+if (Date.now() & 9 > 5) throw new Error()
+
 const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+  parentPort.postMessage(nthFibonacci(workerData))
 };
 
 sendResult();
+
+/*
+*
+* extend given function to work with data received from main thread
+* and implement function which sends result of the computation
+* to the main thread
+*
+*/
